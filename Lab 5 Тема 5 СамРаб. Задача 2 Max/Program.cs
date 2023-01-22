d20f32ec6657d5bfd5f2b0b5526cae187a96fd1a
Maris Shapiro <19maris72@gmail.com>
2023-01-22 14:30:18 +0300

    Добавьте файлы проекта.

---
+++ Lab 5 Тема 5 СамРаб. Задача 2 Max/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Lab_5_Тема_5_СамРаб._Задача_2_Max
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = new int[15];
+            Console.WriteLine("Введите число");
+            Random random = new Random();
+            int s = 0;
+            for (int i = 0; i < 15; i++)
+            {
+                array[i] = random.Next(0, 50);
+                s += array[i];
+                Console.Write("{0} ", array[i]);
+            }
+            Console.WriteLine();
+            int max = array[0];
+            foreach (int a in array)
+            {
+                if (a > max) ;
+                max = a;
+            }
+            Console.WriteLine(s);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Lab 5 Тема 5 СамРаб. Задача 2/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Lab_5_Тема_5_СамРаб._Задача_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = new int[15];
+            Console.WriteLine("Введите число");
+            Random random = new Random();
+            int s = 0;
+            for (int i = 0; i < 15; i++)
+            {
+                array[i] = random.Next(0, 50);
+                s += array[i];
+                Console.Write("{0} ", array[i]);
+            }
+            Console.WriteLine();
+            Console.WriteLine(s);
+            Console.ReadKey();
+        }
+    }
+}
